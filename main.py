895d03ecafe0eaeb09c742c95c844b4b3fc29b8c
zenedf <zenedf@protonmail.com>
2025-09-23 16:11:41 -0400

    player(triangle) is now showing in game

--- main.py
+++ main.py
@@ -5,15 +5,23 @@
 ###
 
 import pygame
+from player import Player
 from constants import *
+# from circleshape import CircleShape
+
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0 # Delta time
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    
     while (True):
-        screen.fill((0, 0, 0))  # Clear screen with black
+        screen.fill("black")  # Clear screen with black
+        player.draw(screen)
         pygame.display.flip()   # Refresh the screen
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
