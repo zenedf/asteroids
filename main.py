29d77f0fd30e21e9756fa19b520f08a26644db2f
zenedf <zenedf@protonmail.com>
2025-09-23 13:38:56 -0400

    closing the window now works

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+###
+# check for player inputs
+# update the game world
+# draw the game to the screen
+###
+
 import pygame
 from constants import *
 
@@ -5,6 +11,13 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while (True):
+        screen.fill((0, 0, 0))  # Clear screen with black
+        pygame.display.flip()   # Refresh the screen
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 
 if __name__ == "__main__":
