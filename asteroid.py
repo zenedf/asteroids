2895bebcb066134a04c568b381134de8b029cef8
zenedf <zenedf@protonmail.com>
2025-09-24 12:46:17 -0400

    added a shot rate limit (0.3) seconds

--- asteroid.py
+++ asteroid.py
@@ -5,11 +5,11 @@ from circleshape import CircleShape
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
-        self.__color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.__color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) # random r,g,b
 
     def draw(self, screen):
-        pygame.draw.circle(screen, self.__color, self.position, self.radius)
-        # pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+        # remove the 'width' to fill the circle
+        pygame.draw.circle(screen, self.__color, self.position, self.radius, 2)
         
     def update(self, dt):
         self.position += self.velocity * dt
