45707a254dfba1bdc9a3cc6be15d2c62e3520d9b
zenedf <zenedf@protonmail.com>
2025-09-24 12:37:45 -0400

    randomized the asteroid color

--- asteroid.py
+++ asteroid.py
@@ -1,12 +1,15 @@
 import pygame
+import random
 from circleshape import CircleShape
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+        self.__color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+        pygame.draw.circle(screen, self.__color, self.position, self.radius)
+        # pygame.draw.circle(screen, "white", self.position, self.radius, 2)
         
     def update(self, dt):
         self.position += self.velocity * dt
--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
         
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -29,8 +30,10 @@ class Player(CircleShape):
     def shoot(self):
         new_shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
         new_shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.timer = PLAYER_SHOOT_COOLDOWN
         
     def update(self, dt):
+        self.timer -= dt
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -41,4 +44,5 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.timer < 0:
+                self.shoot()    
